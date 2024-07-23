6f06ed0383e7fe65fc404daaade8db1151786c51
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-07-23 12:28:23 -0700

    login status completed

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,65 +1,80 @@
+//Routes to process user data
 const router = require('express').Router();
-// Import the User model from the models folder
 const { User } = require('../../models');
 
-// If a POST request is made to /api/users, a new user is created. The user id and logged in state is saved to the session within the request object.
+// Create new user
 router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+    try {
+        const dbUserData = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          adminStatus: false
+        });
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
 
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+
+        // Session set up
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.currentUserId = dbUserData.dataValues.id;
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
-// If a POST request is made to /api/users/login, the function checks to see if the user information matches the information in the database and logs the user in. If correct, the user ID and logged-in state are saved to the session within the request object.
+// Login process
 router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+              email: req.body.email,
+            },
+        });
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
+        if (!dbUserData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password!' });
+            return;
+        }
+
+        // Session set up
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.currentUserId = dbUserData.dataValues.id;
+            req.session.isAdmin = dbUserData.dataValues.adminStatus;
+            res
+                .status(200)
+                .json({ user: dbUserData, message: 'You are logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
     }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
 
-// If a POST request is made to /api/users/logout, the function checks the logged_in state in the request.session object and destroys that session if logged_in is true.
+// Logout
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
